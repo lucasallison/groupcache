25ac56f68c63ad4ce4c9450c64a3b93eef63fcac
Lucas Allison <lucasgallison@gmail.com>
2022-04-07 11:35:29 +0200

    user can now create a new proxy cache

--- proxycache.go
+++ proxycache.go
@@ -13,7 +13,7 @@ import (
 	tagger "github.com/lucasallison/ETagger"
 )
 
-type ProxyFetcher struct {
+type ProxyWrapper struct {
 	Proxy  *httputil.ReverseProxy
 	Writer http.ResponseWriter
 	Req    *http.Request
@@ -26,94 +26,114 @@ type CachedResponse struct {
 	Body       []byte
 }
 
-var et *tagger.ETagger = tagger.NewTagger()
-
-func NewProxyCacheGroup(cacheBytes int64) *Group {
-	return NewGroup("pc", cacheBytes, GetterFunc(
-		func(ctx Context, key string, dest Sink) error {
-			return nil
-		}))
+type ProxyCache struct {
+	etagger *tagger.ETagger
+	group   *Group
 }
 
-// TODO implement context?
-// TODO explain
-func ProxyCacheGet(g *Group, ctx context.Context, key string, pf ProxyFetcher) error {
-	return getRemote(g, ctx, key, pf)
+// TODO name? now only pc ...
+func NewProxyCache(cacheBytes int64) *ProxyCache {
+	pc := ProxyCache{
+		etagger: tagger.NewTagger(nil, false),
+		group: NewGroup("pc", cacheBytes, GetterFunc(
+			func(ctx Context, key string, dest Sink) error {
+				return nil
+			})),
+	}
+	return &pc
 }
 
-// TODO naming
-func getRemote(g *Group, ctx context.Context, key string, pf ProxyFetcher) error {
-	pf.Proxy.ModifyResponse = func(r *http.Response) error {
+func (pc *ProxyCache) Get(ctx context.Context, proxy ProxyWrapper) error {
+
+	key := proxy.Req.URL.Path
+
+	proxy.Proxy.ModifyResponse = func(r *http.Response) error {
 
 		modified := true
 		cachehit := false
-		b := []byte{}
-		dest := AllocatingByteSliceSink(&b)
+		cachedBytes := []byte{}
+		dest := AllocatingByteSliceSink(&cachedBytes)
 
 		/* NOT MODIFIED: try to load from cache */
 		if r.StatusCode == http.StatusNotModified {
 
 			modified = false
+			value, cachehit := pc.group.lookupCache(key)
 
-			value, cacheHit := g.lookupCache(key)
-			if cacheHit {
-				/* CACHE HIT: try to load from cache */
-
+			/* CACHE HIT: try to load from cache */
+			if cachehit {
 				logInfo(cachehit, modified, key)
-				return processCacheHit(r, &b, dest, &value)
+				return pc.processCacheHit(r, &cachedBytes, dest, &value)
 			}
 
 		}
 
 		logInfo(cachehit, modified, key)
 
-		// TODO should we base the etag on the entire request? not just the body?
-		/* update ETag */
-		respbody, err := getBodyAsBytes(r)
-		if err != nil {
-			return err
-		}
-		et.SaveBytesAsTag(key, respbody)
-
 		/* OBJECT MODIFIED OR CACHE MISS: update cache */
-		encResp, err := encodeResponse(r)
-		if err != nil {
-			return err
-		}
-
-		return writeToCache(g, key, dest, *encResp)
+		return pc.modifyCache(dest, key, r)
 	}
 
-	pf.Req.Header.Set("ETag", et.GetEtag(key))
-
-	pf.Proxy.ServeHTTP(pf.Writer, pf.Req)
-	/* dont modify future results e.g. for post requests */
-	pf.Proxy.ModifyResponse = nil
+	pc.serveRequest(key, proxy)
 
 	return nil
 }
 
-// TODO ...
-func logInfo(cachehit bool, modified bool, key string) {
-	if cachehit {
-		log.Println("CACHE HIT! For ", key)
-	} else if !modified {
-		log.Println("CACHE MISS! Updating cache for ", key)
-	} else {
-		log.Println("MODIFIED! Updating cache for ", key)
-	}
+func (pc *ProxyCache) serveRequest(key string, proxy ProxyWrapper) {
+
+	proxy.Req.Header.Set("ETag", pc.etagger.GetEtag(key))
+
+	proxy.Proxy.ServeHTTP(proxy.Writer, proxy.Req)
+
+	/* dont modify future results e.g. for post requests */
+	proxy.Proxy.ModifyResponse = nil
 }
 
-func processCacheHit(r *http.Response, b *[]byte, dest Sink, value *ByteView) error {
+/*
+	modify cache saves the etag and encodes the reponse.
+	writeToCache only writes the supplied bytes to the cache.
+*/
+func (pc *ProxyCache) modifyCache(dest Sink, key string, r *http.Response) error {
 
-	/* will write the bytes of value into b*/
+	// TODO should we base the etag on the entire request? not just the body?
+	/* update ETag */
+	pc.etagger.SaveResponseAsTag(key, r)
+
+	encResp, err := encodeResponse(r)
+	if err != nil {
+		return err
+	}
+
+	return pc.writeToCache(key, dest, *encResp)
+}
+
+func (pc *ProxyCache) writeToCache(key string, dest Sink, b []byte) error {
+	err := dest.SetBytes(b)
+	if err != nil {
+		return err
+	}
+
+	value, err := dest.view()
+	if err != nil {
+		return err
+	}
+
+	// TODO see line 298 of groupecache. We cant only call this function...
+	pc.group.populateCache(key, value, &pc.group.mainCache)
+
+	return nil
+}
+
+func (pc *ProxyCache) processCacheHit(r *http.Response, cachedBytes *[]byte, dest Sink, value *ByteView) error {
+
+	/* will write the bytes of value (cached response) into cachedBytes */
 	err := setSinkView(dest, *value)
 	if err != nil {
 		replaceWithInternalServerError(r)
 		return err
 	}
 
-	cr, err := decodeResponse(*b)
+	cr, err := decodeResponse(*cachedBytes)
 	if err != nil {
 		replaceWithInternalServerError(r)
 		return err
@@ -161,7 +181,6 @@ func decodeResponse(b []byte) (*CachedResponse, error) {
 
 func encodeResponse(r *http.Response) (*[]byte, error) {
 
-	// TODO Do we want to pass this as a parameter?
 	respbody, err := getBodyAsBytes(r)
 
 	/* encode struct */
@@ -196,19 +215,12 @@ func getBodyAsBytes(r *http.Response) (*[]byte, error) {
 	return &respbody, nil
 }
 
-func writeToCache(g *Group, key string, dest Sink, b []byte) error {
-	err := dest.SetBytes(b)
-	if err != nil {
-		return err
+func logInfo(cachehit bool, modified bool, key string) {
+	if cachehit {
+		log.Println("CACHE HIT! For ", key)
+	} else if !modified {
+		log.Println("CACHE MISS! Updating cache for ", key)
+	} else {
+		log.Println("MODIFIED! Updating cache for ", key)
 	}
-
-	value, err := dest.view()
-	if err != nil {
-		return err
-	}
-
-	// TODO see line 298 of groupecache. We cant only call this function...
-	g.populateCache(key, value, &g.mainCache)
-
-	return nil
 }
